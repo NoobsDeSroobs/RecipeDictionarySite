63816aa515816431e408c32e741ce5126b8d5ac2
NoobsDeSroobs <magnus_elden@hotmail.com>
2018-05-05 16:53:34 +0200

    Made some slight style changes and added the first input options

--- RecipeDictionarySite/wwwroot/js/site.js
+++ RecipeDictionarySite/wwwroot/js/site.js
@@ -1,1 +1,8 @@
 ﻿// Write your Javascript code.
+
+function refresh() {
+    confirm('Are you sure you want to save this thing into the database?');
+    setInterval(function () {
+        location.reload();
+    }, 1000);
+};
